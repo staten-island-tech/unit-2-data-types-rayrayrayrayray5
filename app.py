98218e0f60eba4922c5dabd52e3007bd2d257394
rayrayrayrayray5 <raymondl187@nycstudents.net>
2025-02-06 09:23:48 -0500

    challenge and madlib

--- app.py
+++ app.py
@@ -10,8 +10,40 @@ for i in values:
 """ print(values[0])
 print(values[6]) """
 
-x = "this is a thing"
+""" x = "this is a thing"
 y= x.split( )
 z = y[0]
 print(y)
-print(z)
+print(z) """
+
+""" day_of_week = input("what day is it? ")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect") """
+
+""" x = "test"
+print(f"hello {x}") """
+
+
+temp = 67
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold')
+    
+#Challenge
+""" x = input("sentence:")
+y=x.split()
+count = len(y)
+print(count) """
+
+#MADLIB
+a = input("Select a name:")
+b = input("Select a verb:")
+c = input("Select a place:")
+
+
+print(a was )
