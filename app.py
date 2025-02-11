326adfaee5138f63737a668317f3d2fb0cdf138a
rayrayrayrayray5 <raymondl187@nycstudents.net>
2025-02-11 09:19:47 -0500

    finished gcf

--- app.py
+++ app.py
@@ -82,5 +82,10 @@ for i in range(1, number+1):
         print(i) """
 
 #Determine GCF
-
-
+number = int(input('enter a number:'))
+number2 = int(input('enter a number:'))
+gcf = 1
+for i  in range(1, min(number,number2)+1):
+    if number%i == 0 and number2%i == 0:
+        gcf = i
+print(gcf)
